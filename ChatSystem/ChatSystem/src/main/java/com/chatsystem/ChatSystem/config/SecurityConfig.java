7997d776670c4cb753bd5bba53128c8343ef6be7
TshiamoMogolane <tshiamomogolane695@gmail.com>
2026-06-19 14:55:26 +0200

    added a security configuration

--- ChatSystem/ChatSystem/src/main/java/com/chatsystem/ChatSystem/config/SecurityConfig.java
+++ ChatSystem/ChatSystem/src/main/java/com/chatsystem/ChatSystem/config/SecurityConfig.java
@@ -2,12 +2,16 @@ package com.chatsystem.ChatSystem.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -30,10 +34,11 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource())) // 👈 activate CORS
                 .csrf(csrf -> csrf.disable()) // only if you don't need CSRF for signup
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll() // allow signup/login without auth
                         .anyRequest().authenticated()
-                );
+                ).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class); ;
         return http.build();
     }
 
@@ -55,5 +60,10 @@ public class SecurityConfig {
         provider.setPasswordEncoder(passwordEncoder);
         provider.setUserDetailsService(userDetailsService);
         return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
