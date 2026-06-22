95e01e4e828d78269ad509921a3e566b970a6e5b
TshiamoMogolane <tshiamomogolane695@gmail.com>
2026-06-22 12:13:36 +0200

    created a model class for PasswordResetToken

---
+++ ChatSystem/ChatSystem/src/main/java/com/chatsystem/ChatSystem/model/PasswordResetToken.java
@@ -0,0 +1,67 @@
+package com.chatsystem.ChatSystem.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class PasswordResetToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String token;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    private LocalDateTime expiryDate;
+
+    public PasswordResetToken() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDateTime expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    @Override
+    public String toString() {
+        return "PasswordResetToken{" +
+                "id='" + id + '\'' +
+                ", token='" + token + '\'' +
+                ", user=" + user +
+                ", expiryDate=" + expiryDate +
+                '}';
+    }
+
+}
