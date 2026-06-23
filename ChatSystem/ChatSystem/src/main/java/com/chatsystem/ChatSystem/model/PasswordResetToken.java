0c26f00a94787e0585ceacdd7493c4f8ba56c6b3
TshiamoMogolane <tshiamomogolane695@gmail.com>
2026-06-23 09:54:10 +0200

    fixed a column error

--- ChatSystem/ChatSystem/src/main/java/com/chatsystem/ChatSystem/model/PasswordResetToken.java
+++ ChatSystem/ChatSystem/src/main/java/com/chatsystem/ChatSystem/model/PasswordResetToken.java
@@ -15,7 +15,7 @@ public class PasswordResetToken {
     @Column(nullable = false)
     private String token;
 
-    @Column(nullable = false)
+
     @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
     @JoinColumn(nullable = false, name = "user_id")
     private User user;
